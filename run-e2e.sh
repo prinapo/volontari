55d8c69709b8facc81a380583d13da30bb26e1c4
Giovanni Prinetti <giovanni.prinetti@gmail.com>
2026-07-19 11:03:12 +0000

    chore(tests): silent nvm output in E2E script
    Closes #32

--- run-e2e.sh
+++ run-e2e.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-nvm use 24.14.1
+nvm use 24.14.1 --silent
 cd /home/ubuntu/volontari
 npx playwright test --config tests/e2e/playwright.config.js --project=chromium --project=mobile 2>&1
 echo "EXIT: $?"
